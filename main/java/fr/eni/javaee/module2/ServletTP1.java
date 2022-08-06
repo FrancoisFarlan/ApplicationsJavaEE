ab42374c1eb218dd3a1e17df054d77d41f319a13
FrancoisFarlan <francois.farlan@gmail.com>
2022-08-06 09:22:31 +0200

    amelioration TP nBTireAuSort : ajout parametrages des bornes dans la Servlet

--- main/java/fr/eni/javaee/module2/ServletTP1.java
+++ main/java/fr/eni/javaee/module2/ServletTP1.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Random;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,12 +13,26 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletTP1
  */
-@WebServlet("/tps/nbTireAuSort/ServletTP1")
+@WebServlet(urlPatterns = "/tps/nbTireAuSort/ServletTP1", initParams = {
+		@WebInitParam(description = "borne inférieure tirage au sort", name = "BORNE_INF", value = "1"),	
+		@WebInitParam(description = "borne supérieure tirage au sort", name = "BORNE_SUP", value = "10")
+	})
 public class ServletTP1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	Random rand = new Random();
 
+	
+	private String borneInf; 
+    private String borneSup;
+    
+	
+	@Override
+	public void init() throws ServletException {
+		this.borneInf = this.getInitParameter("BORNE_INF");
+		this.borneSup = this.getInitParameter("BORNE_SUP");
+		super.init();
+	}
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
@@ -25,7 +40,7 @@ public class ServletTP1 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		int value = rand.nextInt(10) + 1;
+		int value = rand.nextInt((Integer.parseInt(this.borneSup) - Integer.parseInt(this.borneInf)) + 1) + Integer.parseInt(this.borneInf);
 		System.out.println(value);
 		int nombre = Integer.parseInt(request.getParameter("nombre"));
 		System.out.println(nombre);
